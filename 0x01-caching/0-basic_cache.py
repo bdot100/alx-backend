7948195f534f846e66731fd39b73acdc46801c05
bdot100 <bidemiokunade@gmail.com>
2023-08-23 13:59:22 +0100

    Basic chache class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Class inherits from BaseCaching and is a chaning system
+    """
+    def __init__(self):
+        """
+        Lets Initialize the class using the parent
+        class __init__ method
+        """
+        BaseCaching.__init__(self)
+        
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        the item value for the key key.
+        Args:
+            Key
+            Item
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+            
+    def get(self, key):
+        """
+        return the value in self.cache_data
+        linked to key.
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
