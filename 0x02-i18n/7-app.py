7a4a8e7d608dd524c67b69abc37cf20c98f3045a
bdot100 <bidemiokunade@gmail.com>
2023-08-30 20:12:05 +0100

    get_locale function added

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -83,7 +83,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """the / route is handled here"""
-    return render_template('6-index.html')
+    return render_template('7-index.html')
 
 
 if __name__ == '__main__':
